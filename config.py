da6cffbcde394a71b0037c3a1108551c166ad21f
Ronald Kimera <rkimera2013@gmail.com>
2021-07-25 21:26:35 +0300

    python data pipeline setup

--- config.py
+++ config.py
@@ -12,38 +12,9 @@ DB_DETAILS = {
     'source': {
         'DB_TYPE': 'postgres',
         'DB_NAME': environ.get('DB_DATABASE_NAME'),
-        'DB_HOST': 'localhost',
-        'DB_USER':'postgres',
+        'DB_HOST': environ.get('DB_HOST'),
+        'DB_USER': environ.get('DB_USERNAME'),
         'DB_PASS': environ.get('DB_PASSWORD'),
-
-    },
-    'target': {
-        'DB_TYPE': 'postgres',
-        'DB_NAME': os.environ.get('DB_TARGET'),
-        'DB_HOST': os.environ.get('DB_HOST'),
-        'DB_PORT': os.environ.get('DB_PORT'),
-        'DB_USER': os.environ.get('DB_USERNAME'),
-        'DB_PASS': os.environ.get('DB_PASSWORD')
-    },
-}
-
-
-DB_DETAILS_ENV = {
-    'dev': {
-        'DB_TYPE': 'mysql',
-        'DB_NAME': '',
-        'DB_HOST': '',
-        'DB_NAME': '',
-        'DB_USER': '',
-        'DB_PASS': ''
-    },
-    'source': {
-        'DB_TYPE': 'postgres',
-        'DB_NAME': os.environ.get('DB_DATABASE_NAME'),
-        'DB_HOST': os.environ.get('DB_HOST'),
-        'DB_PORT': os.environ.get('DB_PORT'),
-        'DB_USER': os.environ.get('DB_USERNAME'),
-        'DB_PASS': os.environ.get('DB_PASSWORD')
 
     },
     'target': {
